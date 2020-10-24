6db0bc9c1c3da7d6cdc8694ba1d29c0e62ba0916
tholakelebusi <tholakelebusi@gmail.com>
2020-10-24 10:27:33 +0100

    2 files modified, 7 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,36 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import {
+  MatToolbarModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatCardModule
+} from "@angular/material";
+import { RegisterComponent } from "../register/register.component";
+import { LoginComponent } from "../login/login.component";
+import { AddproductComponent } from "../addproduct/addproduct.component";
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule
+  ],
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    RegisterComponent,
+    LoginComponent,
+    AddproductComponent
+  ],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
